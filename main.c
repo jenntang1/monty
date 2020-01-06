36f456d57ec7a2211b0868d8c509f90f860e17f4
Gzoref <gzoref@gmail.com>
2020-01-06 13:53:17 -0500

    Fix open file

--- main.c
+++ main.c
@@ -18,9 +18,9 @@ int main(int argc, char *argv[])
 	ssize_t get_line;
 	void (*operator_function)(stack_t **stack, unsigned int line_number);
 
-	file = fopen(argv[1], "r");
 	if (argc != 2)
 		fprintf(stderr, "USAGE: monty file\n"), exit(EXIT_FAILURE);
+	file = fopen(argv[1], "r");
 	if (file == NULL)
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]), exit(EXIT_FAILURE);
 	while (1)
