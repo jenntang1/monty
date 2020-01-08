cdd5176b71283d4f9d8eea3124b0bce3aee95ed2
jenntang1 <1039@holbertonschool.com>
2020-01-08 22:48:07 +0000

    Updating to correct error message, jenntang1

--- get_sub.c
+++ get_sub.c
@@ -15,7 +15,7 @@ void get_sub(stack_t **stack, unsigned int line_number)
 
 	if ((*stack == NULL) || ((*stack)->next == NULL))
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		fclose(file);
 		get_free(*stack);
 		exit(EXIT_FAILURE);
